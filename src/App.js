34e8df04e8d05fad49c0e0e2a7989bf526209169
pengpai <815566704>
2018-12-13 09:50:21 +0800

    312

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {observer} from 'mobx-react';
 
 @observer
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                123
-            </div>
+            <Fragment className="ApeSing">
+                ApeSing
+            </Fragment>
         );
     }
 }
